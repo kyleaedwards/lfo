c986a8f08727546609d446c8255a92497e521104
kyleaedwards <edwards.kyle.a@gmail.com>
2018-11-09 09:50:49 -0600

    Update deploy script and ignore build files

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,11 +1,16 @@
 # Update/install node modules
 yarn
 
-# Generate public files
+# # Generate public files
 yarn build
 
 # Remove node modules
 rm -rf ./node_modules
 
+# Retrieve EB environment label from config
+CURRENT_DIR=$(dirname "$0")
+EB_CONFIG="$CURRENT_DIR/../.elasticbeanstalk/config.yml"
+EB_ENV="$(cat "$EB_CONFIG" | sed -n -e "s/^.*environment: //p")"
+
 # Deploy to Elastic Beanstalk
-eb deploy
+eb deploy "$EB_ENV"
