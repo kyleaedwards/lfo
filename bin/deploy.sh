6f14bc6183b0b9719da0f9c16be3bc33889ec9c3
kyleaedwards <edwards.kyle.a@gmail.com>
2018-11-09 12:32:18 -0600

    Ignore build, and add Dockerrun.aws.json

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,5 +12,9 @@ CURRENT_DIR=$(dirname "$0")
 EB_CONFIG="$CURRENT_DIR/../.elasticbeanstalk/config.yml"
 EB_ENV="$(cat "$EB_CONFIG" | sed -n -e "s/^.*environment: //p")"
 
+docker build -t kedwio .
+docker tag kedwio:latest 605028258457.dkr.ecr.us-east-1.amazonaws.com/kedwio:latest
+docker push 605028258457.dkr.ecr.us-east-1.amazonaws.com/kedwio:latest
+
 # Deploy to Elastic Beanstalk
 eb deploy "$EB_ENV"
